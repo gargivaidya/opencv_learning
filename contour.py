6d98f6aca8ea6c59976dc15d4f569c83dd742137
Gargi Vaidya <gargivaidya@tamu.edu>
2021-07-11 20:31:02 -0500

    Detect contours

---
+++ contour.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('./photos/city.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+# canny = cv.Canny(blur, 125, 175)
+
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+
+#  Returns al list of contours and hierarchical representation of contours
+# RETR_EXTERNAL - returns external contours, RETR_TREE - retruns hierarchical contours under a contour
+# Approximation method - 
+contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+print(len(contours))
+
+cv.waitKey(0)
